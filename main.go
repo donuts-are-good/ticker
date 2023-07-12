8493b04f5ef623c3a97c9588f5188efe34b6655a
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2023-07-11 21:05:38 -0500

    scrolling RSS

--- main.go
+++ main.go
@@ -1,27 +1,67 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/mmcdole/gofeed"
 )
 
-var demoStrings = []string{
-	"..  Opinion: Massive floods have swallowed up the Vermont town I love",
-	"..  $20 gift cards and $1 books: GOP primary candidates test novel ways to raise money as they scramble for a spot on next month’s debate stage",
-	"..  Russian commander killed while jogging may have been tracked on Strava app",
-	"..  Opinion: What Sen. Tommy Tuberville waffling on White nationalism really means",
-	"..  Justice Department takes unusual step to try to protect Trump from testifying in lawsuit over FBI firing",
-	"..  WAF Awards 2023: World’s best new buildings unveiled",
-	"..  Home Run Derby: Vladimir Guerrero Jr. follows in father’s footsteps to win",
+const feedsFile = ".tickerfeeds.list"
+
+type Item struct {
+	PubDate time.Time
+	Title   string
+	Link    string
 }
 
 func main() {
-	// Join all the demoStrings into a single string
-	joinedString := strings.Join(demoStrings, " ")
+	// Get the path to the feeds file in the XDG home directory
+	feedsFilePath, err := getFeedsFilePath()
+	if err != nil {
+		fmt.Println("Failed to get feeds file path:", err)
+		return
+	}
+
+	var items []Item
+	for _, feedURL := range rssFeeds {
+		fp := gofeed.NewParser()
+		feed, err := fp.ParseURL(feedURL)
+		if err != nil {
+			fmt.Printf("Failed to fetch RSS feed from %s: %v\n", feedURL, err)
+			continue
+		}
+
+		for _, item := range feed.Items {
+			pubDate, err := parsePublishedTime(item.Published)
+			if err != nil {
+				fmt.Printf("Failed to parse published time from %s: %v\n", item.Published, err)
+				continue
+			}
+
+			items = append(items, Item{PubDate: pubDate, Title: item.Title, Link: item.Link})
+		}
+	}
+
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].PubDate.Before(items[j].PubDate)
+	})
+
+	for _, item := range items {
+		headline := fmt.Sprintf("%s \"%s\" %s  ..  ", item.PubDate.Format("2006-01-02 15:04:05"), item.Title, item.Link)
+		headlines = append(headlines, headline)
+	}
+
+	// Join all the headlines into a single string
+	joinedString := strings.Join(headlines, "")
 
 	// Get the terminal width to fit the scrolling text
 	terminalWidth, err := getTerminalWidth()
@@ -37,10 +77,8 @@ func main() {
 	for {
 		// Scroll the joined string
 		for i := 0; i <= len(joinedString); i++ {
-			// Clear the terminal
-			cmd := exec.Command("clear") // For Windows, replace "clear" with "cls"
-			cmd.Stdout = os.Stdout
-			cmd.Run()
+			// Clear the terminal based on the operating system
+			clearTerminal()
 
 			// Build the scrolling line
 			var scrollingLine string
@@ -54,12 +92,41 @@ func main() {
 			fmt.Print(scrollingLine)
 
 			// Sleep for a short duration to control the scrolling speed
-			time.Sleep(100 * time.Millisecond)
+			time.Sleep(200 * time.Millisecond)
 		}
-
-		// Sleep for a longer duration before scrolling again
-		time.Sleep(500 * time.Millisecond)
 	}
+}
+
+// getFeedsFilePath returns the path to the feeds file in the XDG home directory
+func getFeedsFilePath() (string, error) {
+	homeDir, err := homedir.Dir()
+	if err != nil {
+		return "", err
+	}
+	return homeDir + "/.config/" + feedsFile, nil
+}
+
+// readRSSFeeds reads the RSS feeds from the given file path
+func readRSSFeeds(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var feeds []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		feed := strings.TrimSpace(scanner.Text())
+		if feed != "" && !strings.HasPrefix(feed, "#") {
+			feeds = append(feeds, feed)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return feeds, nil
 }
 
 // getTerminalWidth retrieves the width of the terminal window
@@ -72,4 +139,18 @@ func getTerminalWidth() (int, error) {
 	}
 	width := strings.TrimSpace(string(out))
 	return strconv.Atoi(width)
+}
+
+// clearTerminal clears the terminal based on the operating system
+func clearTerminal() {
+	switch runtime.GOOS {
+	case "windows":
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	default:
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
 }
